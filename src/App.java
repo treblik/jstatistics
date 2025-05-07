47063a60fce27974740c6f84be32878e01f78dab
treblik <regikillbert@gmail.com>
2025-05-07 11:05:24 -0300

    Jogo Java

--- src/App.java
+++ src/App.java
@@ -1,69 +1,54 @@
-import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import java.util.Random;
 
-import javax.swing.JOptionPane;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("lógica-Aula5");
-        // cara ou coroa
-        // variável que receba a iteração do jogo
-        //Ramdom
-        //switch
-        //for ,do while e while
-        /*
-         * public class CaraOuCoroa {
 
-          
-        // espaço amostral
-         */
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        String resposta;
+        int contPar = 0;
+        int contImpar =0;
+        
+        do {
+            int resultado = random.nextInt(6) + 1; // Número aleatório de 1 a 6
+           //Mostra mensagem inicial
+           String mensagem ="Espaço amostral:{1,2,3,4,5,6}\n"+
+        "Probabilidade de numero par:50%\n" +
+        "Probabilidade de numero impar 50% \n" +
+         "Deseja lançar o Dado";
+         JOptionPane.showConfirmDialog(null, mensagem,"Jogar Dados",JOptionPane.YES_NO_OPTION);
+            // Mostra o resultado do lançamento
+            JOptionPane.showMessageDialog(null, "Você lançou o dado e saiu: " + resultado);
 
-         //ArrayList lista =new ArrayList<>();
-        // sorteio
-         Random random = new Random();
-         // criar variável contadores para verificar a distribuição
+            // Pergunta se o usuário deseja lançar novamente
+            resposta = JOptionPane.showInputDialog("Deseja lançar o dado novamente? (s/n)");
 
-      int resultado=0;
-        String mensagem ="Espaço amostral:{1,2,3,4,5,6}\n"+
-                        "Probabilidade de numero par:50%\n" +
-                        "Probabilidade de numero impar 50% \n" +
-                         "Deseja lançar o Dado";
-        // loop de lançamento
-
-        while (true) {
-            // interação com o usuário   
-            int opcao = JOptionPane.showConfirmDialog(null, mensagem,"Jogar Dados",JOptionPane.YES_NO_OPTION);
-            if(opcao!=JOptionPane.YES_NO_OPTION){
-                // estatisticas
-                String estatisticas = "Jogo finalizado!\n" ;
-               // "Caras:"+contCara+"\n"+ 
-                //"Coroas:"+contCoroa;
-                JOptionPane.showMessageDialog(null, estatisticas);
+            if (resposta == null) { // Se o usuário fechar a janela
                 break;
             }
-            int resultado = random.nextInt(6);// Lançamento do dado
-        
-         String[]espacoAmostral ={"1","2","3","4","5","6"};
-                  switch () {
-                    case 1:
-                        resultado;
-                        break;
-                    case 2:
-                        resultado;
-                    case 3:
-                        resultado;
-                    case 4:
-                        resultado;
-                    case 5:
-                        resultado;
-                    case 6:
-                        resultado;
-                    default:
-                        break;
-                  }
+
+            resposta = resposta.trim().toLowerCase();
+
+        } while (resposta.equals("s"));
+
+        JOptionPane.showMessageDialog(null, "Fim do programa. Obrigado por jogar!");
+       
+        //Contador de par ou impar
+       
+       /*  String[]espacoAmostral ={"1","2","3","4","5","6"};
+                  int resultado;
+             String mensagem = "Você lançou o dado e saiu: " + resultado;
+        if(resultado % 2 == 0){
+            contPar++;
+           
+         }else{
+            contImpar++;
+               
+         }
           
          String saida = "Resultado do lançamento"+espacoAmostral[resultado]+
                         "\n (indice gerado:"+resultado+")";
-                        JOptionPane.showMessageDialog(null,  saida, "Resultado",JOptionPane.INFORMATION_MESSAGE);
-        }
+                        JOptionPane.showMessageDialog(null,  saida, "Resultado",JOptionPane.INFORMATION_MESSAGE);*/
     }
 }
