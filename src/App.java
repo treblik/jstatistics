77e189b106ea10d36f3fb40ea747ce01853b4b33
treblik <regikillbert@gmail.com>
2025-05-07 08:35:39 -0300

    pr commite

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Git");
+        System.out.println("Aula 5");
+        /*
+         * Criar um jogo de lançar dados similar ao jogo de cara e coroa
+         * Estudar perquisar e escolher um dos métodos estatísticos
+         * P(a)calculo simples
+         */
     }
 }
