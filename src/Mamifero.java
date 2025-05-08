716690d9b3963c99537ee83ecf01b65f4be6ea92
treblik <regikillbert@gmail.com>
2025-05-08 11:39:06 -0300

    Retorno void

---
+++ src/Mamifero.java
@@ -0,0 +1,22 @@
+public class Mamifero {
+     
+    // atributos
+    private int id;
+    private String nomeCientifico;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+
+
+
+}
